92e824d012acc1765c2f771535a261227cc83f0a
iancorbett <iancorbett1324@gmail.com>
2025-10-21 22:39:38 -0700

    signup page is now working, will now create the forms to sign up and login

--- Ledger.Web/obj/Debug/net9.0/Ledger.Web.AssemblyInfo.cs
+++ Ledger.Web/obj/Debug/net9.0/Ledger.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcafc4228855415261926ef046281ba3e82fee50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca840f67da3931f04d38e4d1ffa686bc3dee7bbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
