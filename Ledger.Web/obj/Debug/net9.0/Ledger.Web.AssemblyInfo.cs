21cef97172dea2d3d8d2b1a9e79eb9bcab93bea7
iancorbett <iancorbett1324@gmail.com>
2025-10-27 16:58:08 -0700

    troubleshooting may need a client side program file

--- Ledger.Web/obj/Debug/net9.0/Ledger.Web.AssemblyInfo.cs
+++ Ledger.Web/obj/Debug/net9.0/Ledger.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a1e9f003fef4b2e0087ca8426ca822011c2bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50da81b6a5ab7c4a663f697947d3043033d3fe45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
