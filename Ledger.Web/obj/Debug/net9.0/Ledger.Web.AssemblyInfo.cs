b3715508e85405f711dad9e4dd0524ec82674764
iancorbett <iancorbett1324@gmail.com>
2025-10-28 12:40:16 -0700

    fixed routing errors for links between login and signup pages

--- Ledger.Web/obj/Debug/net9.0/Ledger.Web.AssemblyInfo.cs
+++ Ledger.Web/obj/Debug/net9.0/Ledger.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f74cdfda7015f496e512efa5b3ba81f281530f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4476fd2ebbb724c0f233e58a737e4cb926c41a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ledger.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
