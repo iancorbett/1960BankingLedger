954d489ba7bd94b98a54c1291b78d084b2d9c4d9
iancorbett <iancorbett1324@gmail.com>
2025-10-28 22:14:31 -0700

    change user id from int to a string in transaxction file

--- Ledger.Web/Data/Transaction.cs
+++ Ledger.Web/Data/Transaction.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Ledger.Web.Identity;
 
 namespace Ledger.Web.Data;
 
@@ -6,11 +8,23 @@ public class Transaction
 {
     public int Id { get; set; }
 
-    [Required] public int UserId { get; set; }
-    public User? User { get; set; }
+    //  Use string because IdentityUser.Id is string (GUID)
+    [Required]
+    public string UserId { get; set; } = "";
 
-    [Required] public DateTime Date { get; set; } = DateTime.UtcNow.Date;
-    [Required] public string Payee { get; set; } = "";
-    public string Memo { get; set; } = "";
-    [Required] public decimal Amount { get; set; } // +credit, -debit
+    //  Navigation property to ApplicationUser
+    [ForeignKey(nameof(UserId))]
+    public ApplicationUser? User { get; set; }
+
+    [Required]
+    public DateTime Date { get; set; } = DateTime.UtcNow.Date;
+
+    [Required, MaxLength(100)]
+    public string Payee { get; set; } = "";
+
+    [MaxLength(200)]
+    public string? Memo { get; set; } = "";
+
+    [Required, Column(TypeName = "numeric(12,2)")]
+    public decimal Amount { get; set; } // +credit, -debit
 }
