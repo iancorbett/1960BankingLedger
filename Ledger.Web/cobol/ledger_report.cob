b479f3fea17e2c6741ea7a4efb7e6c5fc5b28dfc
iancorbett <iancorbett1324@gmail.com>
2025-11-02 16:17:43 -0800

    createenvurooonment divisioni in cobol file

--- Ledger.Web/cobol/ledger_report.cob
+++ Ledger.Web/cobol/ledger_report.cob
@@ -2,3 +2,11 @@
        *> Usage:  ledger_report <input.csv> <output.txt>
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LEDGER-REPORT.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT INFILE ASSIGN TO DYNAMIC infile-name
+               ORGANIZATION LINE SEQUENTIAL.
+           SELECT OUTFILE ASSIGN TO DYNAMIC outfile-name
+               ORGANIZATION LINE SEQUENTIAL.
