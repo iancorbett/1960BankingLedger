18f02a8b680514f3bc937629fd56e39a23c809fb
iancorbett <iancorbett1324@gmail.com>
2025-11-02 16:18:20 -0800

    create data division

--- Ledger.Web/cobol/ledger_report.cob
+++ Ledger.Web/cobol/ledger_report.cob
@@ -10,3 +10,12 @@
                ORGANIZATION LINE SEQUENTIAL.
            SELECT OUTFILE ASSIGN TO DYNAMIC outfile-name
                ORGANIZATION LINE SEQUENTIAL.
+
+        DATA DIVISION.
+       FILE SECTION.
+       FD  INFILE.
+       01  IN-REC                 PIC X(512).
+
+       FD  OUTFILE.
+       01  OUT-REC                PIC X(256).
+
